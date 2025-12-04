9fd18aa39f9fceead0453bb767d6cb8f3046270f
Christie Ngo <christiecngo@gmail.com>
2025-12-03 22:21:09 -0800

    updated cross validation method and data transformations

--- Code/fullcode.R
+++ Code/fullcode.R
@@ -1,6 +1,7 @@
 setwd("C:\\Users\\chris\\Documents\\MAT 261A\\Project 2\\Data")
 
 df = read.csv("Trans Athletes DATA.csv")
+df = subset(df, (margin_of_victory != 1) & (margin_of_victory != 0)) # removing uncontested elections
 rp = subset(df, republican == 1)
 
 dm = subset(df, democrat == 1)
@@ -28,7 +29,6 @@ library(reshape2)
 ## Compute correlation matrix
 #cor_mat_dm <- cor(dm_num, use = "pairwise.complete.obs")
 #cor_mat_rp <- cor(rp_num, use = "pairwise.complete.obs")
-
 
 
 # Transformation into logit because of clear horizontal line at proportion = 1 
@@ -132,8 +132,81 @@ dm$AllRelig = NULL # not specific nor a proportion out of pop
 dm$mmd = NULL # shows median marginal district 
 
 
+dm <- na.omit(dm) # drop 8 rows
+rp <- na.omit(rp) # drop 19 rows
+
 allvars_dm <- lm(margin_of_victory ~ ., data = dm)
 allvars_rp <- lm(margin_of_victory ~ ., data = rp)
+
+
+par(mfrow = c(1, 2))
+plot(allvars_dm, ,which = 1:2, main="Diagnostic Plots for Democrat Full Model")
+
+plot(allvars_rp, which=1:2, main="Diagnostic Plots for Republican Full Model")
+
+library(MASS)
+boxcox(allvars_rp)
+boxcox(allvars_dm)
+
+# boxcox reveals best lambda is 0.5
+library(dplyr)
+dm <- dm %>%
+  mutate(margin_of_victory_sqrt = sqrt(margin_of_victory))
+
+rp <- rp %>%
+  mutate(margin_of_victory_sqrt = sqrt(margin_of_victory))
+
+allvars_dm <- lm(margin_of_victory_sqrt ~ ., data = dm)
+allvars_rp <- lm(margin_of_victory_sqrt ~ ., data = rp)
+
+
+# residual plots still show a U-curve
+par(mfrow = c(1, 2))
+plot(allvars_dm, ,which = 1:2, main="Diagnostic Plots for Democrat Full Model")
+
+plot(allvars_rp, which=1:2, main="Diagnostic Plots for Republican Full Model")
+
+resids_dm <- residuals(allvars_dm)
+resids_rp <- residuals(allvars_rp)
+
+predictor_columns_dm <- names(dm)[c(1:7,9:21)]
+predictor_columns_rp <- names(rp)[c(1:7,9:21)]
+
+set.seed(11)
+par(mfrow = c(3, 3), mar = c(4, 4, 1, 1)) # Set up a 3x3 plotting grid
+for (col_name in predictor_columns_dm) {
+  plot(dm[[col_name]], resids_dm, 
+       xlab = col_name, 
+       ylab = "Residuals", 
+       main = paste("Democrat Residuals vs.", col_name))
+  # Add a smoothed line (lowess) to highlight the pattern
+  lines(lowess(dm[[col_name]], resids_dm), col = "red", lwd = 2) 
+}
+
+for (col_name in predictor_columns_rp) {
+  plot(rp[[col_name]], resids_rp, 
+       xlab = col_name, 
+       ylab = "Residuals", 
+       main = paste("Republican Residuals vs.", col_name))
+  # Add a smoothed line (lowess) to highlight the pattern
+  lines(lowess(rp[[col_name]], resids_rp), col = "red", lwd = 2) 
+}
+
+par(mfrow = c(1, 1))
+
+transformed_model_poly_dm <- lm(
+  margin_of_victory_sqrt ~ poly(SSMC, 2) + poly(EP, 2) + . - margin_of_victory, # includes all other untransformed columns
+  data = dm)
+
+transformed_model_poly_rp <- lm(
+  margin_of_victory_sqrt ~ poly(pctbachelordeg, 2) + poly(rural,2) + 
+    poly(SSMC,2) + poly(EP,2) + . - margin_of_victory, # includes all other untransformed columns
+  data = rp)
+
+par(mfrow = c(1, 2))
+plot(transformed_model_poly_dm, which = 1:2)
+plot(transformed_model_poly_rp, which = 1:2)
+par(mfrow = c(1, 1))
 
 aic_step_dm = step(allvars_dm, direction="both")
 aic_step_rp = step(allvars_rp, direction="both")
@@ -141,80 +214,111 @@ aic_step_rp = step(allvars_rp, direction="both")
 model_full_dm <- lm(margin_of_victory ~ ., data = dm)
 model_full_rp <- lm(margin_of_victory ~ ., data = rp)
 
-
-library(car)
-vif(aic_step_dm)
-vif(aic_step_rp)
-
-
-library(MASS)
-library(glmnet)
-
-dm <- na.omit(dm) # drop 8 rows
-rp <- na.omit(rp) # drop 19 rows
-
-
-X_dm =  model.matrix(margin_of_victory ~ ., data = dm)[, -1] # drop added intercept
-y_dm = dm$margin_of_victory
-
-ridge_res_dm <- glmnet(X_dm, y = y_dm, alpha = 0, nfolds=10) # alpha = 0 for ridge
-lasso_res_dm <- glmnet(X_dm, y = y_dm, alpha = 1, nfolds=10) # alpha = 1 for LASSO
-
-X_rp =  model.matrix(margin_of_victory ~ ., data = rp)[, -1] # drop added intercept
-y_rp = rp$margin_of_victory
-
-ridge_res_rp <- glmnet(X_rp, y = y_rp, alpha = 0, nfolds=10) # alpha = 0 for ridge
-lasso_res_rp <- glmnet(X_rp, y = y_rp, alpha = 1, nfolds=10) # alpha = 1 for LASSO
-
-# SELECTED lambda
-
-cv_lasso$lambda.min
-cv_ridge$lambda.min
-
-ridge_res_dm <- glmnet(X_dm, y = y_dm, alpha = 0, nfolds=10, lambda = ridge_res_dm$lambda.min) # alpha = 0 for ridge
-lasso_res_dm <- glmnet(X_dm, y = y_dm, alpha = 1, nfolds=10, lambda = lasso_res_dm$lambda.min) # alpha = 1 for LASSO
-
-ridge_res_rp <- glmnet(X_rp, y = y_rp, alpha = 0, nfolds=10, lambda=ridge_res_rp$lambda.min) # alpha = 0 for ridge
-lasso_res_rp <- glmnet(X_rp, y = y_rp, alpha = 1, nfolds=10, lambda=lasso_res_rp$lambda.min) # alpha = 1 for LASSO
-
-coef(ridge_res_dm)
-
-coef(lasso_res_dm)
-
-coef(ridge_res_rp)
-
-coef(lasso_res_rp)
-
-
 library("tidymodels")
-# 80% train, 10% validation, 10% test
-dm_split <- initial_validation_split(dm, prop = c(0.8, 0.10)) 
+# 80% train, 0% validation, 20% test -> will use cross validation 10-folds
+dm_split <- initial_validation_split(dm, prop = c(0.8, 0.0, 0.10)) 
 
 # Extract the individual datasets
 dm_train <- training(dm_split)
 dm_validation <- validation(dm_split)
 dm_test <- testing(dm_split)
 
-rp_split <- initial_validation_split(rp, prop = c(0.8, 0.10)) 
+rp_split <- initial_validation_split(rp, prop = c(0.8, 0.0, 0.10)) 
 
 # Extract the individual datasets
 rp_train <- training(rp_split)
 rp_validation <- validation(rp_split)
 rp_test <- testing(rp_split)
 
-library(leaps)
-best_subsets_mod_dm <- regsubsets(margin_of_victory ~ ., data = dm, nvmax = 10)
-best_subsets_summary_dm <- summary(best_subsets_mod_dm)
-plot(1:10, best_subsets_summary_dm$adjr2,
-     type = "l",
-     xlab = "Number of predictors",
-     ylab = "Adjusted r-squared")
-points(1:10, best_subsets_summary_dm$adjr2)
 
-best_subsets_mod_rp <- regsubsets(margin_of_victory ~ ., data = rp, nvmax = 10)
-best_subsets_summary_rp <- summary(best_subsets_mod_rp)
-plot(1:10, best_subsets_summary_rp$adjr2,
-     type = "l",
-     xlab = "Number of predictors",
-     ylab = "Adjusted r-squared")
-points(1:10, best_subsets_summary_rp$adjr2)
+library(MASS)
+library(glmnet)
+library(caret)
+
+dm_final_formula = margin_of_victory_sqrt ~ poly(SSMC, 2) + poly(EP, 2) + . - margin_of_victory
+rp_final_formula = margin_of_victory_sqrt ~ poly(pctbachelordeg, 2) + poly(rural,2) + 
+  poly(SSMC,2) + poly(EP,2) + . - margin_of_victory
+
+# use trainControl function from caret, use RMSE as the metric
+ctrl <- trainControl(
+  method = "cv", # cross-validation 
+  number = 10 # 10-Fold CV
+)
+
+model_full_dm <- train(
+  dm_final_formula,
+  data = dm,
+  method = "lm", # Standard Linear Model
+  trControl = ctrl,
+  metric="RMSE"
+)
+
+
+model_full_rp <- train(
+  rp_final_formula,
+  data = rp,
+  method = "lm", # Standard Linear Model
+  trControl = ctrl,
+  metric="RMSE"
+)
+
+lasso_model_dm <- train(
+  dm_final_formula,
+  data = dm,
+  method = "glmnet",
+  trControl = ctrl,
+  preProc = c("center", "scale"), # IMPORTANT for LASSO
+  tuneGrid = expand.grid(.alpha = 1, .lambda = 10^seq(-3, 0, length = 50)), # Pure LASSO
+  metric="RMSE"
+)
+
+lasso_model_rp <- train(
+  rp_final_formula,
+  data = rp,
+  method = "glmnet",
+  trControl = ctrl,
+  preProc = c("center", "scale"), # IMPORTANT for LASSO
+  tuneGrid = expand.grid(.alpha = 1, .lambda = 10^seq(-3, 0, length = 50)),
+  metric="RMSE" # Pure LASSO
+)
+
+
+
+model_stepwise_approx_dm <- train(
+  dm_final_formula,
+  data = dm,
+  method = "leapSeq", # Sequential Search (Forward/Backward)
+  trControl = ctrl,
+  tuneGrid = expand.grid(.nvmax = 1:(ncol(dm) - 1)),
+  metric="RMSE" # Test models with 1 to N-1 predictors
+)
+
+
+model_stepwise_approx_rp <- train(
+  rp_final_formula,
+  data = rp,
+  method = "leapSeq", # Sequential Search (Forward/Backward)
+  trControl = ctrl,
+  tuneGrid = expand.grid(.nvmax = 1:(ncol(rp) - 1)),
+  metric="RMSE" # Test models with 1 to N-1 predictors
+)
+
+results_dm <- resamples(list(
+  Full_Model = model_full_dm,
+  LASSO_Model = lasso_model_dm,
+  Stepwise_Cp = model_stepwise_approx_dm
+))
+
+results_rp <- resamples(list(
+  Full_Model = model_full_rp,
+  LASSO_Model = lasso_model_rp,
+  Stepwise_Cp = model_stepwise_approx_rp
+))
+
+# Summarize the average performance (Mean and SD of RMSE)
+summary(results_dm)
+summary(results_rp)
+
+# Visualize the comparison (lower box means better RMSE)
+bwplot(results_dm, metric = "RMSE")
+bwplot(results_rp, metric = "RMSE")
